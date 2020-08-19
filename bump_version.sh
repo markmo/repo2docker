9d7eb93088036041b7b1890fcecf482cab533101
Mark Moloney <markmo@me.com>
2020-08-19 12:47:24 +1000

    fix bump_version script

--- bump_version.sh
+++ bump_version.sh
@@ -3,7 +3,8 @@
 profile=${1}
 ENV=${2:-test}
 
-if [ "$ENV" == "test" ]
+# `sh` uses a single eq
+if [ "$ENV" = "test" ]
 then
     bin/versionbump patch repo2docker/buildpacks/profiles/${profile}/TEST-VERSION
     git add repo2docker/buildpacks/profiles/${profile}/TEST-VERSION
